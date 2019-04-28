0f33df0ce2d84c0c19d78a807d33672abe0f0b29
JoacyMS <joacymsilva@gmail.com>
2019-04-28 18:42:33 -0300

    Alternância de Jogador e Exibição dos Símbolos

--- tictactoe/js/tictactoe.js
+++ tictactoe/js/tictactoe.js
@@ -1,17 +1,46 @@
 const tic_tac_toe = {
     tabuleiro: ['', '', '', '', '', '', '', '', ''],
-    simbolos: ['X', 'O'],
+    simbolos: { 
+        options: ['X', 'O'],
+        turn_index: 0,
+        change: function(){
+            this.turn_index = (this.turn_index === 0 ? 1 : 0);
+        }
+    },
     container_element: null,
+    gameover: false,
+    winning_sequences: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ],
 
     init: function (container) {
         this.container_element = container;
+    },
+
+    make_play: function(posicao){
+        if(this.gameover){
+            return false;
+        }
+
+        if(this.tabuleiro[posicao] === ''){
+            this.tabuleiro[posicao] = this.simbolos.options[this.simbolos.turn_index];
+            this.draw();
+            this.simbolos.change();
+        }
     },
 
     draw: function () {
         let content = '';
 
         for(i in this.tabuleiro){
-            content += '<div class="col-4">' + this.tabuleiro[i] + '</div>';
+            content += '<div onclick="tic_tac_toe.make_play(' + i + ')" class="col-4">' + this.tabuleiro[i] + '</div>';
         }
 
         this.container_element.innerHTML = content;
